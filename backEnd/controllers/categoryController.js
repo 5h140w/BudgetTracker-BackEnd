eaa156e7623d1b7a60c8c926345a15e9d9e8fcb6
5h140w <5h140w23@gmail.com>
2022-06-29 17:11:25 +0100

    category post

--- backEnd/controllers/categoryController.js
+++ backEnd/controllers/categoryController.js
@@ -5,3 +5,27 @@ module.exports.getAllCategories = async(req,res) =>{
     const categories = await Category.find()
     return res.status(200).json(categories)
 }
+
+
+module.exports.addCategory = async(req,res) =>{
+    const {name, description} = req.body
+
+    const cat= await Category.findOne({name: name.toLowerCase()})
+    if(cat) return res.status(404).json({"msg":"name shoumd be unique"})
+
+    const newCategory = new Category({
+        name : name.toLowerCase(),
+        description : description
+    })
+
+    newCategory.save().then(
+        ()=>{
+            return res.status(200).json({"msg":"category added successfully!"})
+        }
+    ).catch(
+        (err)=>{
+            return res.status(404).json({"msg":err.message})
+        }
+    )
+
+}
