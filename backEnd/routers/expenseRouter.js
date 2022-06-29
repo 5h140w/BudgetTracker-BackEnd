87de5a537e73f4d5383f54acd43c45fcf18d3d0b
5h140w <5h140w23@gmail.com>
2022-06-29 20:29:09 +0100

    expense router

--- backEnd/routers/expenseRouter.js
+++ backEnd/routers/expenseRouter.js
@@ -4,6 +4,6 @@ const expenseController = require("../controllers/expenseController")
 const router = Router()
 
 router.get("/all", expenseController.getAllExpenses)
-
+router.get("/:id", expenseController.getExpenseById)
 
 module.exports = router
