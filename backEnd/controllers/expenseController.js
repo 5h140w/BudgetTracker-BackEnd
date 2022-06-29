83fbc2fb189101bd7952a49cf4b000e17c3d9b6f
5h140w <5h140w23@gmail.com>
2022-06-29 20:28:20 +0100

    expense controller

--- backEnd/controllers/expenseController.js
+++ backEnd/controllers/expenseController.js
@@ -5,3 +5,21 @@ module.exports.getAllExpenses = async(req,res) =>{
     const expenses = await Expense.find()
     return res.status(200).json(expenses)
 }
+
+
+module.exports.getExpenseById = async(req,res) =>{
+    const {id} = req.params
+    
+    Expense.findOne({_id:id})
+        .then(
+            (data)=>{
+                return res.status(200).json(data)
+            }
+        )
+        .catch(
+            (err)=>{
+                return res.status(404).json({"msg":err.message})
+            }
+        )
+
+}
