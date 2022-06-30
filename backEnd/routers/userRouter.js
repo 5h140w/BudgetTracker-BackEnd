50e413e8abc063f9bbacbfeb0d98d572513a9e98
5h140w <5h140w23@gmail.com>
2022-06-30 08:44:07 +0100

    user Router

--- backEnd/routers/userRouter.js
+++ backEnd/routers/userRouter.js
@@ -3,6 +3,8 @@ const userController = require("../controllers/userController")
 
 const router = Router()
 
+router.get("/all", userController.getAllUsers)
+router.get("/:id",userController.getUserById)
 router.put("/changeData/:id",userController.changeData)
 
 module.exports = router
