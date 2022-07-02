29070c9330da1111924e67ebd798b51399320c3f
5h140w <5h140w23@gmail.com>
2022-07-02 14:25:04 +0100

    router

--- backEnd/routers/expenseRouter.js
+++ backEnd/routers/expenseRouter.js
@@ -5,8 +5,12 @@ const router = Router()
 
 router.get("/all", expenseController.getAllExpenses)
 router.get("/:id", expenseController.getExpenseById)
+router.get("/user/:user", expenseController.getUserExpenses)
+router.get("/totalamount/:user", expenseController)
 router.post("/",expenseController.addExpense)
 router.delete("/",expenseController.deleteAll)
 router.delete("/:id",expenseController.deleteExpense)
+
+
 
 module.exports = router
