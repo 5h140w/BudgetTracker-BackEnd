57509a66a6e2e8450f2b9777b36ad3011f47eb72
5h140w <5h140w23@gmail.com>
2022-07-04 19:12:53 +0100

    homepage

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -6,7 +6,15 @@ const HomePage = () =>{
             <div className='home_container'>
                 <div className='home'>
                     <div className="left_section">
-
+                        <div className="top">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/7/71/Black.png"/>
+                            <h3>Shady Belhadj</h3>
+                        </div>
+                        <ul>
+                            <li>Home</li>
+                            <li>Deposit</li>
+                            <li></li>
+                        </ul>
                     </div>
                     <div className="right_section">
 
