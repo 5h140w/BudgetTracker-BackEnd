d0846c397e4964c34c604719329b0d2e83189e63
5h140w <5h140w23@gmail.com>
2022-07-04 18:15:45 +0100

    homepafge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import{
 import LoginPage from './pages/loginPage/LoginPage';
 import RegisterPage from './pages/registerPage/RegisterPage';
 import WelcomePage from './pages/WelcomePage/WelcomePage';
+import HomePage from './pages/homePage/HomePage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<WelcomePage/>}/> 
         <Route path='/login' element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./HomePage.css"
+import "./homepage.css"
 
 const HomePage = () =>{
         return(
