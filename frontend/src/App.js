45a5ba0dff02755a37821fba2212b538f6067d54
5h140w <5h140w23@gmail.com>
2022-07-05 16:25:13 +0100

    hompepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <> </>
+    <div className="home_container">
+      <div className="left">
+        <div className="top">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwDAxAPFD7OhGWmsVEi7NkZMI17n-1rBX3tg&usqp=CAU" alt="avatar"/>
+            <h4>Shady</h4>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="right">
+
+      </div>
+    </div>
   );
 }
 
