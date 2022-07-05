407e5508f6c3d53917c44e46239b6197e2fae3b3
5h140w <5h140w23@gmail.com>
2022-07-05 16:45:46 +0100

    change sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faBaby, faCoins, faDashboard, faGears, faHome, faMoneyBill } from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   return (
@@ -11,7 +13,34 @@ function App() {
         <div className="menu">
           <ul>
             <li>
-              
+              <a href="/">
+                <FontAwesomeIcon icon={ faHome }/>
+                <span>Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FontAwesomeIcon icon={ faMoneyBill }/>
+                <span>Deposits</span>
+              </a>
+            </li>
+             <li>
+              <a href="/">
+                <FontAwesomeIcon icon={ faCoins }/>
+                <span>Expenses</span>
+              </a>
+            </li>
+             <li>
+              <a href="/">
+                <FontAwesomeIcon icon={ faDashboard }/>
+                <span>Dashboard</span>
+              </a>
+            </li>
+             <li>
+              <a href="/">
+                <FontAwesomeIcon icon={ faGears }/>
+                <span>Settings</span>
+              </a>
             </li>
           </ul>
         </div>
