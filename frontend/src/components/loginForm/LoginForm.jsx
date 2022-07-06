ada3a59ac521ed5ed23bce5821ef9d08041d1a99
5h140w <5h140w23@gmail.com>
2022-07-06 16:24:16 +0100

    loginForm

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Box from '@mui/material/Box';
 
 const LoginForm = () =>{
     return(
-        <></>
+        <>
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                    />
+            </Box>
+        </>
     )
 }
 
