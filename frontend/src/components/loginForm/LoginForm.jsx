2824970601f3cb948507246afaafe7e6108b343d
5h140w <5h140w23@gmail.com>
2022-07-06 17:33:52 +0100

    loginPage

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const LoginForm = () =>{
+    const handle = (e) =>{
+        e.preventDefault()
+        console.log("ok")
+    }
     return(
-        <>
+        <div 
+            style={{ display:"flex",
+                justifyContent:"center",
+                alignItems:"center",
+                minHeight:"100vh"
+            }}
+        > 
             <Box
                 component="form"
                 sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                   
+                    width:"700px",
                 }}
                 noValidate
                 autoComplete="off"
-            >
-                <TextField
-                    id="standard-number"
-                    label="Number"
-                    type="number"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="standard"
+                onSubmit={handle}
+            > 
+                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+                    <Input
+                        id="standard-adornment-amount"
+                        value={"21213"}
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
                     />
+                </FormControl>
             </Box>
-        </>
+        </div>
     )
 }
 
