05656a6862ba773be9b1134d64d0b8915c5a3de9
5h140w <5h140w23@gmail.com>
2022-07-08 12:34:12 +0100

    register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import {BrowserRouter , Routes , Route} from "react-router-dom"
+import RegisterForm from './components/registerForm/RegisterForm';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<LoginPage/>} />
-          <Route path="/register" element={"ok"} />
+          <Route path="/register" element={<RegisterForm/>} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/registerForm/RegisterForm.jsx
+++ frontend/src/components/registerForm/RegisterForm.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Box, FormControl , TextField, Button, Typography} from '@mui/material';
+
 
 const RegisterForm = () =>{
+
+    const [username, setusername] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+    const [error, seterror] = useState("")
+
     return(
-        <></>
+       <Box
+            sx={{
+                p : 3
+            }}
+        > 
+            <Box
+                component="form"     
+                noValidate
+                autoComplete="off"
+            > 
+                <FormControl 
+                    fullWidth={true} 
+                >
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Username"
+                        margin="normal"
+                        variant="standard"
+                        sx={{m : 2}}
+                        onChange={(e)=>setusername(e.target.value)}
+                    />
+
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Email"
+                        type="email"
+                        margin="normal"
+                        variant="standard"
+                        sx={{m : 2}}
+                        onChange={(e)=>setemail(e.target.value)}
+                    />
+                    <TextField 
+                        required
+                        id="standard-required"
+                        label="Password"
+                        type="password"
+                        variant="standard"
+                        margin="normal" 
+                        sx={{m : 2}}
+                        onChange={(e)=>setpassword(e.target.value)}
+                    />
+                    <Button
+                        type='submit'
+                        variant="contained"
+                        sx={{m : 2}}
+                    >
+                        Submit
+                    </Button>
+                    <Typography variant="p" align='center' mt={2} sx={{color:"red", fontSize:15 }}>
+                        {error}
+                    </Typography>
+                </FormControl>
+            </Box>
+        </Box>
     )  
 }
 
