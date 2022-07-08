eb50eaa4112bdbd86419209c2284360032a2af2e
5h140w <5h140w23@gmail.com>
2022-07-08 12:39:33 +0100

    register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 import {BrowserRouter , Routes , Route} from "react-router-dom"
-import RegisterForm from './components/registerForm/RegisterForm';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<LoginPage/>} />
-          <Route path="/register" element={<RegisterForm/>} />
+          <Route path="/register" element={<RegisterPage/>} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import RegisterForm from '../../components/registerForm/RegisterForm'
+import {Box , Typography} from "@mui/material"
+import "./registerpage.css"
 
 const RegisterPage = () =>{
     return(
-        <></>
+        <div className='registerpage_body'>
+            <Box 
+                sx={{
+                    width:"400px",
+                    border:1,
+                    borderRadius : "16px",
+                    borderColor : 'grey.500'
+                }}
+            >
+                <Typography variant='h3' color="black" align="center" sx={{mt: 4}}> Register</Typography> 
+                <RegisterForm/>
+            </Box>
+        </div>
     )
 }
 
