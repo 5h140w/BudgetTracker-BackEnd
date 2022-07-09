1f28dfc8e7f2da9aa65db2a123489c72cc80e7c8
5h140w <5h140w23@gmail.com>
2022-07-09 18:58:35 +0100

    font awesome

--- frontend/src/components/sideBar/SideBar.jsx
+++ frontend/src/components/sideBar/SideBar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faHome} from "@fortawesome/free-solid-svg-icons"
 import "./sidebar.css"
 
 const SideBar = () =>{
@@ -8,6 +10,14 @@ const SideBar = () =>{
                 <img src="https://thumbs.dreamstime.com/b/profil-masculin-sans-visage-d-avatar-138078117.jpg" alt="logo"/>
                 <h3>Name Family</h3>
             </section>
+            <ul className='menu_section'>
+                <li>
+                    <a href="/login">
+                        <FontAwesomeIcon icon={faHome}/>
+                        Home
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
