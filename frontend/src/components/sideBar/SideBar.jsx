1d2d5d05bc3a77b4e71708c7b7959194157ebde7
5h140w <5h140w23@gmail.com>
2022-07-09 17:53:42 +0100

    sidebar

--- frontend/src/components/sideBar/SideBar.jsx
+++ frontend/src/components/sideBar/SideBar.jsx
@@ -3,9 +3,12 @@ import "./sidebar.css"
 
 const SideBar = () =>{
     return(
-        <section className='sidebar_section'>
-            
-        </section>
+        <div className='sidebar_section'>
+            <section className='gn_section'>
+                <img src="" alt="logo"/>
+                <h3>Name Family</h3>
+            </section>
+        </div>
     )
 }
 
