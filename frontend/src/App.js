9ab8354d786a4e7cb49b9d6847ce772851cd1cd1
5h140w <5h140w23@gmail.com>
2022-07-09 11:58:15 +0100

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<LoginPage/>} />
           <Route path="/register" element={<RegisterPage/>} />
+          <Route path="/home" element={"okk"}/>
         </Routes>
       </BrowserRouter>
     </>
