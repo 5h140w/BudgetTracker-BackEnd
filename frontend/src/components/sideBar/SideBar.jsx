1532796d5e490ef39b869d7d8910b3dae2c57ed0
5h140w <5h140w23@gmail.com>
2022-07-09 18:02:29 +0100

    sidebar

--- frontend/src/components/sideBar/SideBar.jsx
+++ frontend/src/components/sideBar/SideBar.jsx
@@ -5,7 +5,7 @@ const SideBar = () =>{
     return(
         <div className='sidebar_section'>
             <section className='gn_section'>
-                <img src="" alt="logo"/>
+                <img src="https://thumbs.dreamstime.com/b/profil-masculin-sans-visage-d-avatar-138078117.jpg" alt="logo"/>
                 <h3>Name Family</h3>
             </section>
         </div>
