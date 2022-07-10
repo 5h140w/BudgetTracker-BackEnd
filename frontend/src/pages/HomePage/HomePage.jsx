b4a89874577de6c48f51a78dd7b9c1328229bf0b
5h140w <5h140w23@gmail.com>
2022-07-10 19:10:13 +0100

    homepage

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import "./homepage.css"
 import SideBar from "../../components/sideBar/SideBar"
+import { Box, Card, CardContent,Typography } from '@mui/material'
 
 const HomePage = () =>{
     return(
         <>
             <SideBar/>
             <div className='main'>
-                <p>klqdhfsjfs</p>
+                <h1>Welcome User</h1>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        justifyContent:"center",
+                        p: 1,
+                        m: 1,
+                        bgcolor: 'background.paper',
+                        borderRadius: 1,
+                    }}
+                >
+                    <Card sx={{width:"400px", m:2}}>
+                        <CardContent>
+                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                                Total Expenses
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card sx={{width:"400px", m:2}}>
+                        <CardContent>
+                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                                Total Deposits
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card sx={{width:"400px", m:2}}>
+                        <CardContent>
+                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                                Balance
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Box>
             </div>
         </>
     )
