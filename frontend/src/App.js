fb923f3e8ac6bdbd7f20f3803334db26836b728e
5h140w <5h140w23@gmail.com>
2022-07-13 20:08:58 +0100

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
-import {BrowserRouter , Routes , Route} from "react-router-dom"
+import {BrowserRouter , Routes , Route , Navigate} from "react-router-dom"
 import OverViewPage from './pages/HomePage/OverViewPage';
 import ExpensesPage from './pages/ExpensesPage/ExpensesPage';
 
@@ -10,10 +10,16 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path="/register" element={<RegisterPage/>} />
-          <Route path="/overview" element={<OverViewPage/>}/>
-          <Route path="/expenses" element={<ExpensesPage/>} />
+          { localStorage.getItem("token")?
+            <>
+              <Route path="/overview" element={<OverViewPage/>}/>
+              <Route path="/expenses" element={<ExpensesPage/>} />
+            </>:
+            <>
+              <Route path='/login' element={<LoginPage/>} />
+              <Route path="/register" element={<RegisterPage/>} />
+            </>
+          }
         </Routes>
       </BrowserRouter>
     </>
