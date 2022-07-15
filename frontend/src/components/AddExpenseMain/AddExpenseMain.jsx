499e8ea0545f2541787c0a1e87a547c3d473026d
5h140w <5h140w23@gmail.com>
2022-07-15 11:03:41 +0100

    add expense main

--- frontend/src/components/AddExpenseMain/AddExpenseMain.jsx
+++ frontend/src/components/AddExpenseMain/AddExpenseMain.jsx
@@ -1,26 +1,24 @@
-import { Box, Typography } from '@mui/material'
+import { Box, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const AddExpenseMain = () =>{
     return(
         <div className='main'>
-            <Box
-                sx={{
-                    m:1,
-                    p:1
-                }}
+            <Box sx={{ m:1, p:1 }}
             >
                 <Typography component={"h1"} sx={{fontWeight:"1000", fontSize:32}}>
                     Add Expense
                 </Typography>
             </Box>
-            <Box
-                sx={{
-                    p:2,
-                    m:1
-                }}
-            >
-                
+            <Box sx={{ p:2, m:1 }}>
+                <Box component="form">
+                    <TextField
+                        label={"Text Value"} //optional
+                    />
+                    <TextField
+                        label={"Text Value"} //optional
+                    />
+                </Box>
             </Box>
         </div>
     )
