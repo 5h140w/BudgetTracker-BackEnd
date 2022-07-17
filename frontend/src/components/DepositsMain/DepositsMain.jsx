32c4c593e81d6dfc3b894992a4f9540a005e2987
5h140w <5h140w23@gmail.com>
2022-07-17 08:34:37 +0100

    DepositsMain

--- frontend/src/components/DepositsMain/DepositsMain.jsx
+++ frontend/src/components/DepositsMain/DepositsMain.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Box, Typography,Button} from "@mui/material"
 
 const DepositsMain = () =>{
     return(
         <div className='main'>
-            
+            <Box
+                    sx={{
+                        m:1,
+                        p:1
+                    }}
+                >
+                    <Typography component={"h1"} sx={{ fontWeight :"1000", fontSize:32}}>
+                        Deposits
+                    </Typography>
+                    <Typography component={"p"} sx={{fontWeight:"600", fontSize:12,p:1, display:"block",float:"right"}}>
+                        XX days
+                    </Typography>
+                    <Button size="small" variant="contained">Add expense </Button>
+            </Box> 
         </div>
     )
 }
