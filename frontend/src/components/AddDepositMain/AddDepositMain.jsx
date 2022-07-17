94fb052ddc63014e87bb28554ec5a2e9eee0830d
5h140w <5h140w23@gmail.com>
2022-07-17 10:46:54 +0100

    addDepositMain

--- frontend/src/components/AddDepositMain/AddDepositMain.jsx
+++ frontend/src/components/AddDepositMain/AddDepositMain.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {Box, Typography} from "@mui/material"
+import {Box, Typography , FormControl,Button, TextField, MenuItem } from "@mui/material"
 
 const AddDepositMain = ()=>{
+    const categories =[]
     return(
         <div className="main">
             <Box sx={{ m:1, p:1 }}>
@@ -9,7 +10,55 @@ const AddDepositMain = ()=>{
                     Add Deposit
                 </Typography>
             </Box>
+            <Box sx={{ p:2, m:1 }}>
+                <Box component="form" sx={{margin:"auto", width:700, p:2, backgroundColor:"#fafafa", borderRadius: 5}}>
+                    <FormControl fullWidth={true} margin="dense" sx={{p:1 , backgroundColor:"white"}}>
+                        <TextField
+                            label="Name"
+                            placeholder="Enter the expense's name"
+                            variant="standard"
+                        />
+                    </FormControl>
+                    <FormControl fullWidth={true} margin="dense" sx={{p:1, backgroundColor:"white"}}>
+                        <TextField
+                            type="number"
+                            inputProps={{
+                                step: "any"
+                            }}
+                            label="Amount"
+                            placeholder="Enter the expense's amount"
+                            variant="standard"
+                        />
+                    </FormControl>
+                    <FormControl fullWidth={true} margin="dense" sx={{p:1, backgroundColor:"white"}}>
+                        <TextField
+                            select
+                            label="Category"
+                            helperText="Please select the expense's category"
+                            variant="standard"
+                        >
+                        {categories.map((option) => (
+                            <MenuItem key={option._id} value={option._id}>
+                                {option.name}
+                            </MenuItem>
+                        ))}
+                        </TextField>
+                    </FormControl>
+                    <FormControl fullWidth={true} margin="dense" sx={{p:1, backgroundColor:"white"}}>
+                        <TextField
+                            label="Description"
+                            multiline
+                            rows={4}
+                            variant="standard"
+                        />
+                    </FormControl>
 
+                    <Button variant="contained" type="submit" sx={{my:2}}>
+                        Submit
+                    </Button>
+
+                </Box>
+            </Box>
         </div>
     )
 }
