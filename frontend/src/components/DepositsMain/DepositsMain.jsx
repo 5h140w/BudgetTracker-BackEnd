c344936ff1c3e3ea2e7fd16d7adfeb7d8b2cb173
5h140w <5h140w23@gmail.com>
2022-07-17 10:16:12 +0100

    fix

--- frontend/src/components/DepositsMain/DepositsMain.jsx
+++ frontend/src/components/DepositsMain/DepositsMain.jsx
@@ -21,69 +21,69 @@ const DepositsMain = () =>{
                     <Button size="small" variant="contained">Add Deposits</Button>
             </Box>
             <Box
-                    sx={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent:"center",
-                        p: 1,
-                        m: 1,
-                        bgcolor: 'background.paper',
-                        borderRadius: 1,
-                    }}
-                >
-                    <Card sx={{width:"500px", m:2, p:2}}>
-                        <CardContent>
-                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
-                                Total Deposits
-                            </Typography>
-                            <Typography sx={{ my:"20px",fontSize: 12, float:"right" }}color="text.secondary">
-                                ... TND 
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                    <Card sx={{width:"500px", m:2, p:2}}>
-                        <CardContent>
-                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
-                                Total Deposits Per Month
-                            </Typography>
-                            <Typography sx={{ my:"20px",fontSize: 12, float:"right" }}color="text.secondary">
-                                ... TND 
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Box>
-                <Box component="div" sx={{ p :2}}>
-                    <Typography sx={{ fontWeight:"600", fontSize: 14, mb:2 }}color="text.secondary">
-                        Last transactions
-                    </Typography>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                            <TableRow>
-                                <TableCell> ID </TableCell>
-                                <TableCell align="right">Name</TableCell>
-                                <TableCell align="right">Amount</TableCell>
-                                <TableCell align="right">Description</TableCell>
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent:"center",
+                    p: 1,
+                    m: 1,
+                    bgcolor: 'background.paper',
+                    borderRadius: 1,
+                }}
+            >
+                <Card sx={{width:"500px", m:2, p:2}}>
+                    <CardContent>
+                        <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                            Total Deposits
+                        </Typography>
+                        <Typography sx={{ my:"20px",fontSize: 12, float:"right" }}color="text.secondary">
+                            ... TND 
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card sx={{width:"500px", m:2, p:2}}>
+                    <CardContent>
+                        <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                            Total Deposits Per Month
+                        </Typography>
+                        <Typography sx={{ my:"20px",fontSize: 12, float:"right" }}color="text.secondary">
+                            ... TND 
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Box>
+            <Box component="div" sx={{ p :2 }}>
+                <Typography sx={{ fontWeight:"600", fontSize: 14, mb:2 }}color="text.secondary">
+                    Last transactions
+                </Typography>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell> ID </TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Amount</TableCell>
+                            <TableCell align="right">Description</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {data.map((row,index) => (
+                            <TableRow
+                            key={row._id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                            <TableCell component="th" scope="row">
+                                {index}
+                            </TableCell>
+                            <TableCell align="right">{row.name}</TableCell>
+                            <TableCell align="right">{row.amount} TND</TableCell>
+                            <TableCell align="right">{row.description}</TableCell>
                             </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {data.map((row,index) => (
-                                <TableRow
-                                key={row._id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                <TableCell component="th" scope="row">
-                                    {index}
-                                </TableCell>
-                                <TableCell align="right">{row.name}</TableCell>
-                                <TableCell align="right">{row.amount} TND</TableCell>
-                                <TableCell align="right">{row.description}</TableCell>
-                                </TableRow>
-                            ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Box>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </div>
     )
 }
