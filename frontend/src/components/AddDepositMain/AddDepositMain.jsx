11829ebc514c56165372118b3f0170cdccd6ee6b
5h140w <5h140w23@gmail.com>
2022-07-17 10:41:09 +0100

    addDeposit

---
+++ frontend/src/components/AddDepositMain/AddDepositMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddDepositMain = ()=>{
+    return(
+        <div className="main">
+            <Box sx={{ m:1, p:1 }}>
+                <Typography component={"h1"} sx={{fontWeight:"1000", fontSize:32}}>
+                    Add Deposit
+                </Typography>
+            </Box>
+
+        </div>
+    )
+}
+
+export default AddDepositMain
