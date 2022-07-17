bfa53ecccecb849218e7c9e09162703c7dce8dab
5h140w <5h140w23@gmail.com>
2022-07-17 19:06:33 +0100

    expense controller

--- backEnd/controllers/expenseController.js
+++ backEnd/controllers/expenseController.js
@@ -16,3 +16,60 @@ module.exports.getExpenseByID = async (req,res)=>{
     
 }
 
+module.exports.addExpense = (req,res) =>{
+    const {name,description,category,amount,user} = req.body
+
+    const newExpense = new Expense({
+        name: name.toLowerCase(),
+        description:description,
+        category:category,
+        amount:amount,
+        user:user,
+        date: new Date().toLocaleDateString(),
+        type:"expense"
+    })
+
+    newExpense.save().then(
+        ()=>{
+            return res.status(200).json({"msg":"Expense Added"})
+        }
+    ).catch(
+        (err)=>{
+            return res.status(404).json({"msg":err.message})
+        }
+    )
+}
+
+
+module.exports.deleteAllExpense = (req,res)=>{
+    const {user} = req.params
+    Expense.deleteMany({user:user})
+        .then(
+            ()=>{
+                return res.status(200).json({"msg":"Expenses are deleted"})
+            }
+        ).catch(
+            (err)=>{
+                return res.status(404).json({"msg":err.message})
+            }
+        )
+}
+
+
+module.exports.deleteByid = (req,res)=>{
+    const {user,id} = req.params
+    Expense.deleteOne({_id:id,user:user})
+        .then(
+            ()=>{
+                return res.status(200).json({"msg":"Expense is deleted"})
+            }
+        ).catch(
+            (err)=>{
+                return res.status(404).json({"msg":err.message})
+            }
+        )
+}
+
+
+
+
