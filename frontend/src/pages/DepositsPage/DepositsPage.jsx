52699204839651e6f9be830ee052d985c248b94c
5h140w <5h140w23@gmail.com>
2022-07-17 08:30:54 +0100

    deposits page

--- frontend/src/pages/DepositsPage/DepositsPage.jsx
+++ frontend/src/pages/DepositsPage/DepositsPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import DepositsMain from '../../components/DepositsMain/DepositsMain'
 import SideBar from '../../components/sideBar/SideBar'
 
 const DepositsPage = () =>{
     return(
         <>
             <SideBar/>
-            
+            <DepositsMain/>
         </>
     )
 }
