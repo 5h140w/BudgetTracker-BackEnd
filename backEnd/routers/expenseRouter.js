7f4ad109affe7068b246bf234774283bca21a8b7
5h140w <5h140w23@gmail.com>
2022-07-17 19:12:05 +0100

    expenserouter

--- backEnd/routers/expenseRouter.js
+++ backEnd/routers/expenseRouter.js
@@ -1,17 +1,12 @@
 const {Router} = require("express")
-const expenseController = require("../controllers/transactionController")
+const expenseController = require("../controllers/expenseController")
 
 const router = Router()
 
-router.get("/all", expenseController.getAllExpenses)
-router.get("/:id", expenseController.getTransactionById)
-router.get("/user/:user", expenseController.getUserExpenses)
-router.get("/totalamount/:user", expenseController.totalamount)
-router.get("/today/:user", expenseController.getExpenseToday)
-router.get("/todayamount/:user", expenseController.todayAmount)
+router.get("/:id", expenseController.getExpenseByID)
+router.get("/user/:user", expenseController.getExpensesByUser)
 router.post("/",expenseController.addExpense)
-router.delete("/",expenseController.deleteAll)
-router.delete("/:id",expenseController.deleteExpense)
-
+router.delete("/user/:user/", expenseController.deleteAllExpense)
+router.delete("/user/:user/:id", expenseController.deleteByid)
 
 module.exports = router
