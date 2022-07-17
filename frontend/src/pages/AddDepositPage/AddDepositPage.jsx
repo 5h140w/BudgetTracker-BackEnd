577a7731d328723344df16e74fc1beb0f61f8275
5h140w <5h140w23@gmail.com>
2022-07-17 10:44:13 +0100

    addDepositMain

--- frontend/src/pages/AddDepositPage/AddDepositPage.jsx
+++ frontend/src/pages/AddDepositPage/AddDepositPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AddDepositMain from '../../components/AddDepositMain/AddDepositMain'
 import SideBar from '../../components/sideBar/SideBar'
 
 const AddDepositPage = () =>{
     return(
         <>
             <SideBar/>
+            <AddDepositMain/>
         </>
     )
 }
