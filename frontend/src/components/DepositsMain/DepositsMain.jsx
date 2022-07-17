bcf79c53a5ee20cf6a9ae34e024b2b744ae290b3
5h140w <5h140w23@gmail.com>
2022-07-17 08:38:40 +0100

    DepositsMain

--- frontend/src/components/DepositsMain/DepositsMain.jsx
+++ frontend/src/components/DepositsMain/DepositsMain.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box, Typography,Button} from "@mui/material"
+import {Box, Typography,Button, Card,CardContent} from "@mui/material"
 
 const DepositsMain = () =>{
 
@@ -20,7 +20,39 @@ const DepositsMain = () =>{
                     </Typography>
                     <Button size="small" variant="contained">Add Deposits</Button>
             </Box>
-            
+            <Box
+                    sx={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        justifyContent:"center",
+                        p: 1,
+                        m: 1,
+                        bgcolor: 'background.paper',
+                        borderRadius: 1,
+                    }}
+                >
+                    <Card sx={{width:"500px", m:2, p:2}}>
+                        <CardContent>
+                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                                Total Deposits
+                            </Typography>
+                            <Typography sx={{ my:"20px",fontSize: 12, float:"right" }}color="text.secondary">
+                                ... TND 
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card sx={{width:"500px", m:2, p:2}}>
+                        <CardContent>
+                            <Typography sx={{ fontWeight:"600", fontSize: 14 }}color="text.secondary">
+                                Total Deposits Per Month
+                            </Typography>
+                            <Typography sx={{ my:"20px",fontSize: 12, float:"right" }}color="text.secondary">
+                                ... TND 
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Box>
+                
         </div>
     )
 }
