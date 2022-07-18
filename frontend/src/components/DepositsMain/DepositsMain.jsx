158cadb808ec470265c7bd9870b3ce3d5b7cf706
5h140w <5h140w23@gmail.com>
2022-07-18 12:15:34 +0100

    deposit main

--- frontend/src/components/DepositsMain/DepositsMain.jsx
+++ frontend/src/components/DepositsMain/DepositsMain.jsx
@@ -86,8 +86,8 @@ const DepositsMain = () =>{
                         <TableBody>
                         {data.map((row,index) => (
                             <TableRow
-                            key={row._id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                key={row._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                             <TableCell component="th" scope="row">
                                 {index}
