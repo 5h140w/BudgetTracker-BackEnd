e2a2a664a413ad4a50ae10b9257df7d0de24f149
5h140w <5h140w23@gmail.com>
2022-07-18 09:43:19 +0100

    change expense controller

--- backEnd/controllers/expenseController.js
+++ backEnd/controllers/expenseController.js
@@ -25,7 +25,7 @@ module.exports.addExpense = (req,res) =>{
         category:category,
         amount:amount,
         user:user,
-        date: new Date().toLocaleString(),
+        date: new Date().toLocaleDateString(),
         type:"expense"
     })
 
