fbadc340bb32352017a536609aef73adb5cdde48
5h140w <5h140w23@gmail.com>
2022-07-20 14:36:40 +0100

    dashboard main

--- frontend/src/components/DashboardMain/DashboardMain.jsx
+++ frontend/src/components/DashboardMain/DashboardMain.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const DashboardMain = () =>{
     return(
         <div className='main'>
-            <Box sx={{p:1}}>
+            <Box sx={{m:1,p:1}}>
                 <h1>Hello There</h1>
             </Box>
         </div>
