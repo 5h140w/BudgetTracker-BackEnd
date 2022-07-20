aab99b07fa066401dc646690f8e7405f0f6bca3c
5h140w <5h140w23@gmail.com>
2022-07-20 14:33:47 +0100

    dashboard main

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import DashboardMain from '../../components/DashboardMain/DashboardMain'
 import SideBar from '../../components/sideBar/SideBar'
 
 const DashboardPage = () =>{
     return(
         <>
             <SideBar/>
+            <DashboardMain/>
         </>
     )
 }
