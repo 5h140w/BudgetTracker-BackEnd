2350aa7224a1a9fce432b75d7dbbb00e53276c4a
5h140w <5h140w23@gmail.com>
2022-07-20 14:35:50 +0100

    dashboard main

--- frontend/src/components/DashboardMain/DashboardMain.jsx
+++ frontend/src/components/DashboardMain/DashboardMain.jsx
@@ -1,9 +1,12 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 const DashboardMain = () =>{
     return(
         <div className='main'>
-
+            <Box sx={{p:1}}>
+                <h1>Hello There</h1>
+            </Box>
         </div>
     )
 }
