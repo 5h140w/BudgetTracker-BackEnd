5bb06e9a9ca2ee153fe1019c6eea26bc577a21f8
5h140w <5h140w23@gmail.com>
2022-07-21 18:56:26 +0100

    dashboard main

--- frontend/src/components/DashboardMain/DashboardMain.jsx
+++ frontend/src/components/DashboardMain/DashboardMain.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import {Box} from "@mui/material"
+import {Box, Typography} from "@mui/material"
 
 const DashboardMain = () =>{
     
     return(
        <div className='main'>
             <Box sx={{p:1}}> 
-                <h1>Dashboard</h1>
+                <Typography component="h1" sx={{ fontWeight :"1000", fontSize:32}}> 
+                    Dashboard
+                </Typography>
+                <Typography component={"p"} sx={{fontWeight:"600", fontSize:12,p:1, display:"block",float:"right"}}>
+                    XX days
+                </Typography>
             </Box>
+
+
        </div>
     )
 }
