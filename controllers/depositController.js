995f343dfb1326587948da627d3b2bb734069159
5h140w <5h140w23@gmail.com>
2022-12-11 14:34:28 +0100

    deposit controller

--- controllers/depositController.js
+++ controllers/depositController.js
@@ -130,8 +130,7 @@ module.exports.getMonthlyDeposit = async (req,res) =>{
     let DepositsMonth =0
     let fromDate = new Date(year , month , 1)
     let toDate = new Date( year, month+1 , 0)
-    console.log(toDate)
-    let Deposits = await Deposit.find({ user:user,type:"Deposit", createdAt :{ "$gte": fromDate , "$lte":toDate}})
+    let Deposits = await Deposit.find({ user:user,type:"deposit", createdAt :{ "$gte": fromDate , "$lte":toDate}})
     Deposits.forEach(Deposit => { DepositsMonth += Deposit.amount});
     return res.status(200).json(DepositsMonth)
 }
