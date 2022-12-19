ed8adb98bbec7ed3f89bd3c1071a824cdd9af70f
5h140w <5h140w23@gmail.com>
2022-12-19 18:00:14 +0100

    db config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-module.exports.connect = () =>{
-    mongoose
-        .connect(
-            "mongodb://localhost:27017/BudgetTracker",{
-                useNewUrlParser:true,
-                useUnifiedTopology: true,
-            }
-        ).then(
-            ()=>{
-                console.log("connected to database")
-            }
-        ).catch(
-            (e)=>{
-                console.log(e)
-                process.exit(1)
-            }
-        )
-}
+module.exports.connect = () => {
+  mongoose
+    .connect("mongodb://localhost:27017/BudgetTracker", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("connected to database");
+    })
+    .catch((e) => {
+      console.log(e);
+      process.exit(1);
+    });
+};
