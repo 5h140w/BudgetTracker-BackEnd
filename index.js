1d3afee808597b08aae3c17dfdf09884eba60fb5
5h140w <5h140w23@gmail.com>
2022-12-19 18:00:24 +0100

    index

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require("express")
-require("./config/database").connect()
-const app = express()
-const cors = require("cors")
-const authRouter = require("./routers/authRouter")
-const userRouter = require("./routers/userRouter")
-const categoryRouter = require("./routers/categoryRouter")
-const expenseRouter = require("./routers/expenseRouter")
-const depositRouter = require("./routers/depositRouter")
-const transactionRouter = require("./routers/transactionRouter")
-const port = process.env.PORT || 8080
+const express = require("express");
+require("./config/database").connect();
+const app = express();
+const cors = require("cors");
+const authRouter = require("./routers/authRouter");
+const userRouter = require("./routers/userRouter");
+const categoryRouter = require("./routers/categoryRouter");
+const expenseRouter = require("./routers/expenseRouter");
+const depositRouter = require("./routers/depositRouter");
+const transactionRouter = require("./routers/transactionRouter");
+const port = process.env.PORT || 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use("/auth" , authRouter)
-app.use("/user",userRouter)
-app.use("/transaction",transactionRouter )
-app.use("/category",categoryRouter)
-app.use("/expense", expenseRouter)
-app.use("/deposit", depositRouter)
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/transaction", transactionRouter);
+app.use("/category", categoryRouter);
+app.use("/expense", expenseRouter);
+app.use("/deposit", depositRouter);
 
-app.listen(port,()=>{
-    console.log("listening to port 8080")
-})
+app.listen(port, () => {
+  console.log("listening to port 8080");
+});
