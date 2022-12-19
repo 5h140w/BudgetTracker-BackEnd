602d4b9304c3bf2238e1a71c36c42f07d4cd04b9
5h140w <5h140w23@gmail.com>
2022-12-19 17:56:51 +0100

    router

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,9 +1,9 @@
-const {Router} = require("express")
-const userCOntroller = require("../controllers/userController")
+const { Router } = require("express");
+const userCOntroller = require("../controllers/userController");
 
-const router = Router()
+const router = Router();
 
-router.post("/login", userCOntroller.login)
-router.post("/register", userCOntroller.register)
+router.post("/login", userCOntroller.login);
+router.post("/register", userCOntroller.register);
 
-module.exports = router
+module.exports = router;
