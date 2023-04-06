d82341d5309c075b614c543ba6f687cfe01d56e8
5h140w <5h140w23@gmail.com>
2023-04-06 12:40:10 +0200

    change code

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,8 +1,12 @@
 const Category = require("../models/category");
 
 module.exports.getAllCategories = async (req, res) => {
-  const categories = await Category.find();
-  return res.status(200).json(categories);
+  try {
+    const categories = await Category.find();
+    return res.status(200).json(categories);
+  } catch (err) {
+    return res.status(404).json({ msg: err });
+  }
 };
 
 module.exports.getCategoryById = async (req, res) => {
